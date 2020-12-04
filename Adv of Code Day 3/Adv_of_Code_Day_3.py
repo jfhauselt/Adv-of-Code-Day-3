993fb95ff2a42518a260da66e00982ad1997d009
John Hauselt <john.hauselt@uconn.edu>
2020-12-04 15:40:01 -0500

    moved to function that takes inputs for right and down moves

--- Adv of Code Day 3/Adv_of_Code_Day_3.py
+++ Adv of Code Day 3/Adv_of_Code_Day_3.py
@@ -1,24 +1,33 @@
 #I know this isnt the right way but ok
-path = r"C:\Users\jfhau\Documents\#code\Day3.txt"
-f = open(path,"r")
-x = 0
-maxX = 0
 
-Trees = 0
+
 
 def slopeCount(down, right):
-    pass
-for line in f:
-    
-    if maxX == 0:
-        maxX = len(line) -2 
+    path = r"C:\Users\jfhau\Documents\#code\Day3.txt"
+    f = open(path,"r")
+    count = down
+    x = 0
+    maxX = 0
+    Trees = 0
 
-    if line[x] == "#":
-        Trees +=1
-    x +=3
-    if x > maxX:
-        x -= maxX +1
+    for line in f:
+        if maxX == 0:
+            maxX = len(line) -2 
+        if count == down:
+            if line[x] == "#":
+                Trees +=1
+            x += right
+            count = 1
+            if x > maxX:
+                x -= maxX +1
+        else:
+            count +=1
+    return Trees
 
 
-
-print(Trees)
+print(slopeCount(1,1))
+print(slopeCount(1,3))
+print(slopeCount(1,5))
+print(slopeCount(1,7))
+print(slopeCount(2,1))
+print(slopeCount(1,1)*slopeCount(1,3)*slopeCount(1,5)*slopeCount(1,7)*slopeCount(2,1))
